0b6237994bed83b56ab6d9772a7e94f1f946f0cd
FabioAguera <fabio.aguera@gmail.com>
2015-03-20 09:41:54 -0300

    Aula 1

---
+++ Ex1_media3.py
@@ -0,0 +1,6 @@
+def media():
+    nota1 = float(raw_input())
+    nota2 = float(raw_input())
+    nota3 = float(raw_input())
+    media = (nota1 + nota2 + nota3)/3.0
+    return media
---
+++ Ex2_par.py
@@ -0,0 +1,9 @@
+def epar():
+    num = float(raw_input())
+    x = num%2
+    if (x == 0):
+        print "é par"
+    else:
+        print "não é par"
+    
+    
